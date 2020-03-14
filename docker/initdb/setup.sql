073055325a630e9b9ea0ea9d022f6c4c1f8ab3f1
Takafumi Yoshida <zephiransas@gmail.com>
2020-03-14 16:21:42 +0900

    ユーザ作成のSQLを修正

--- docker/initdb/setup.sql
+++ docker/initdb/setup.sql
@@ -1,2 +1,3 @@
--- CREATE DATABASE IF NOT EXISTS sbjauth;
-grant all privileges on *.* to todo@'%' identified by 'todo' with grant option;
+-- for local development
+CREATE USER todo IDENTIFIED BY 'todo';
+grant all on `%`.* to 'todo'@'%' identified by 'todo';
