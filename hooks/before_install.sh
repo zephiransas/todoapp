5c048e10f85ca606fc5a514070d051438ce15543
Takafumi Yoshida <zephiransas@gmail.com>
2020-03-14 15:32:39 +0900

    before_installでRAILS_MASTER_KEYをセットするようにする

--- hooks/before_install.sh
+++ hooks/before_install.sh
@@ -5,3 +5,4 @@ export RDS_USERNAME=$(aws --region ap-northeast-1 ssm get-parameter --name '/Tod
 export RDS_PASSWORD=$(aws --region ap-northeast-1 ssm get-parameter --name '/Todoapp/RDS/RDS_PASSWORD' | jq -r '.Parameter.Name')
 export RDS_HOSTNAME=$(aws --region ap-northeast-1 ssm get-parameter --name '/Todoapp/RDS/RDS_HOSTNAME' | jq -r '.Parameter.Name')
 export RDS_PORT=$(aws --region ap-northeast-1 ssm get-parameter --name '/Todoapp/RDS/RDS_PORT' | jq -r '.Parameter.Name')
+export RAILS_MASTER_KEY=$(aws --region ap-northeast-1 ssm get-parameter --name '/Todoapp/RAILS_MASTER_KEY' | jq -r '.Parameter.Name')
