06a41de2c514a3e5ffc1915c6ff1750b0cdf6d88
Takafumi Yoshida <zephiransas@gmail.com>
2020-03-14 17:07:27 +0900

    環境変数の設定をまとめる

--- hooks/start.sh
+++ hooks/start.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-export RDS_DB_NAME=$(aws --region ap-northeast-1 ssm get-parameter --name '/Todoapp/RDS/RDS_DB_NAME' | jq -r '.Parameter.Value')
-export RDS_USERNAME=$(aws --region ap-northeast-1 ssm get-parameter --name '/Todoapp/RDS/RDS_USERNAME' | jq -r '.Parameter.Value')
-export RDS_PASSWORD=$(aws --region ap-northeast-1 ssm get-parameter --name '/Todoapp/RDS/RDS_PASSWORD' | jq -r '.Parameter.Value')
-export RDS_HOSTNAME=$(aws --region ap-northeast-1 ssm get-parameter --name '/Todoapp/RDS/RDS_HOSTNAME' | jq -r '.Parameter.Value')
-export RDS_PORT=$(aws --region ap-northeast-1 ssm get-parameter --name '/Todoapp/RDS/RDS_PORT' | jq -r '.Parameter.Value')
-export RAILS_MASTER_KEY=$(aws --region ap-northeast-1 ssm get-parameter --name '/Todoapp/RAILS_MASTER_KEY' | jq -r '.Parameter.Value')
-
 cd /data/todoapp
+source  ./export_config.sh
 RAILS_ENV=production bundle exec rails s -d
